f82098c45f3ead773456837e7b42b7927a6ebdbf
Armando Carvajal <armandoecr96@gmail.com>
2017-11-03 23:36:40 -0600

    Se agrega la busqueda de funciones en administrarTablas

--- src/administradores/AdministrarTablas.java
+++ src/administradores/AdministrarTablas.java
@@ -25,6 +25,21 @@ public class AdministrarTablas extends MouseAdapter {
 		this.valueObtained = null;
 		this.jTextField = null;
 	}
+	
+	public void cargarBaseDatosFuncion(DefaultTableModel modeloFuncion) throws SQLException {
+		String[] datos = new String[5];
+		
+		Object[][] buscar = new DAO().buscar("idfuncion,idObra,fechaFuncion,inicioFuncion,finalFuncion",DAO.FUNCION);
+		
+		for(int i = 0; i < buscar.length; i++) {
+			datos[0] = buscar[i][0].toString();
+			datos[1] = buscar[i][1].toString();
+			datos[2] = buscar[i][2].toString();
+			datos[3] = buscar[i][3].toString();
+			datos[4] = buscar[i][4].toString();
+			modeloFuncion.addRow(datos);
+		}
+	}
 
 	public void cargarBaseDatosObras(DefaultTableModel modelo) throws SQLException {
 		
